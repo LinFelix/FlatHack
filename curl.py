5684f6120ba5589e7272c78ac81396a274e1a297
vikweg <vwegmayr@inf.ethz.ch>
2016-09-17 05:46:36 +0200

    get images from url

--- curl.py
+++ curl.py
@@ -1,4 +1,7 @@
 import urllib2
+import json
+import Image
+import cStringIO
 
 req = urllib2.Request("https://api-2445581357976.apicast.io:443/rs/real-estates?language=en&chooseType=rentflat&sort=p&page=1&numberResults=10&zip=8052&rentFrom=1500&rentTo=2000&roomsFrom=3&roomsTo=4")
 req.add_header("Accept","application/json")
@@ -6,4 +9,14 @@ req.add_header("auth","7567b660d6cf89544516cda0afc63a38")
 
 r = urllib2.urlopen(req)
 
-print r.read()
+response = r.read()
+
+data = json.loads(response)
+
+for pic in data["items"][2]["pictures"]:
+	req_img = urllib2.Request(pic)
+	res_img = urllib2.urlopen(req_img)
+	img = res_img.read()
+	file = cStringIO.StringIO(img)
+	Image.open(file).show()
+
