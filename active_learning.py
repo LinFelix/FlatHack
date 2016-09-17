9ab15191a14a9a53cbbfd5a6d86de23ec6a40b98
vikweg <vwegmayr@inf.ethz.ch>
2016-09-17 16:28:26 +0200

    finished first try of feedback loop

--- active_learning.py
+++ active_learning.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 # Initialization 
-flats_seen = 0 # id of the flat seen, 0 as the first flat is seen/proposed first in initialization
+flats_seen = [] # id of the flat seen, 0 as the first flat is seen/proposed first in initialization
 sum = np.full((len(flat_cnt),0, dtype = 'float'))
  # sum of all similarities
 iteration_count = 1
@@ -11,24 +11,25 @@ flat_score = np.full((len(flat_cnt),0, dtype = 'float'))
 
 proposed_flat = 0 # the first flat is proposed as no prior knowledge
 
-updated_score = np.full((len(flat_cnt),0, dtype = 'float'))
+flat_score = np.full((len(flat_cnt),0, dtype = 'float'))
 iteration_count += 1
 
 print('User please enter if you liked the proposed flat.')
 user_rating = input('Enter +1 for :) and -1 for :(') # Take the input from user and store it in variable user_rating
 
-while (len(flats_seen) != len(flat_cnt)): # *** insert condition such that loop stops after every flat in the set has been seen
+while (len(flats_seen) != flat_cnt): # *** insert condition such that loop stops after every flat in the set has been seen
 	# insert function that returns sum and flat_sim (flat_similarity vector) here
-	n1 =  # number of images in flat 1
-	n2 =  # number of images in flat 2
-	f1 =  # flat score of flat 1
-	f2 =  # flat score of flat 2 
-
-	updated_score = update_score(flat_score, proposed_flat, user_rating, sum, flat_sim)
+	for f in range(0,flat_cnt):
+		flat_score[f] = update_score(flat_score[f], proposed_flat, f, user_rating, sum[f])
 
 	# propose the next flat as one that has highest score and has not been proposed before
-	proposed_flat = np.where(updated_score == updated_score.max())
 	flats_seen = np.append(flats_seen, proposed_flat)
+	tmp_flat_score = flat_score
+	tmp_flat_score[flats_seen] = -2 
+	proposed_flat = np.where( tmp_flat_score == flat_score.max() )
+
+	print('User please enter if you liked the proposed flat.')
+	user_rating = input('Enter +1 for :) and -1 for :(')
 
 def mi(i,f): # i is the identifier (bathroom/living room ....)
 	return i
@@ -47,13 +48,7 @@ def flat_sim(f1,n1,f2,n2):
 	return sum
 
 # function for updating scores
-def updated_score(flat_score, proposed_flat, user_rating, sum, flat_sim):
-
-	for i in range(0,len(flat_cnt)):
-		if iteration_count == 1:
-			flat_score[i] = user_rating * flat_sim[0]  # flat_sim is the vector that shows similatrity between flats
-		else :
-			flat_score[i] = ((user_rating*flat_sim[proposed_flat]) + flat_score[i])/(sum)
-
-	return flat_score
+def update_score(flat_score, proposed_flat, flat, user_rating, sum):
+	sim = flat_sim( proposed_flat, img_cnt[proposed_flat],  flat, img_cnt[flat])
+	return ( flat_score + user_rating*sim ) / (sum + sim)
 
