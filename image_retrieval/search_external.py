77c3e128624ca6eb2c519cdccd0aa152984da3bd
vikweg <vwegmayr@inf.ethz.ch>
2016-09-17 13:22:22 +0200

    before feedback loop

--- image_retrieval/search_external.py
+++ image_retrieval/search_external.py
@@ -25,7 +25,7 @@ args = vars(ap.parse_args())
 
 # load the query image and show it
 queryImage = cv2.imread(args["query"])
-cv2.imshow("Query", queryImage)
+#cv2.imshow("Query", queryImage)
 print "query: %s" % (args["query"])
 
 # describe the query in the same way that we did in
@@ -43,8 +43,8 @@ results = searcher.search(queryFeatures)
 # we have a total of 25 images in the index, but let's only
 # display the top 10 results; 5 images per montage, with
 # images that are 400x166 pixels
-montageA = np.zeros((166 * 5, 400, 3), dtype = "uint8")
-montageB = np.zeros((166 * 5, 400, 3), dtype = "uint8")
+#montageA = np.zeros((166 * 5, 400, 3), dtype = "uint8")
+#montageB = np.zeros((166 * 5, 400, 3), dtype = "uint8")
 
 # loop over the top ten results
 for j in xrange(0, 10):
@@ -56,14 +56,14 @@ for j in xrange(0, 10):
 	print "\t%d. %s : %.3f" % (j + 1, imageName, score)
 
 	# check to see if the first montage should be used
-	if j < 5:
-		montageA[j * 166:(j + 1) * 166, :] = result
+	#if j < 5:
+	#	montageA[j * 166:(j + 1) * 166, :] = result
 
 	# otherwise, the second montage should be used
-	else:
-		montageB[(j - 5) * 166:((j - 5) + 1) * 166, :] = result
+	#else:
+	#	montageB[(j - 5) * 166:((j - 5) + 1) * 166, :] = result
 
 # show the results
-cv2.imshow("Results 1-5", montageA)
-cv2.imshow("Results 6-10", montageB)
+#cv2.imshow("Results 1-5", montageA)
+#cv2.imshow("Results 6-10", montageB)
 cv2.waitKey(0)
